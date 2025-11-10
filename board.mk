6e2c7fe4a98d8d88ec7f88aa6da2ee8e152866f1
dwisnie2 <101066675+dwisnie2@users.noreply.github.com>
2025-11-10 18:52:00 +0100

    Modify board.mk for memory optimization and features
    Updated DDEFS to optimize memory usage and enable features.

--- board.mk
+++ board.mk
@@ -1,4 +1,5 @@
-include $(BOARD_DIR)/firmware/firmware.mk
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
+    $(BOARD_DIR)/firmware/pinouts.cpp \
 
 
 BOARDINC += $(BOARD_DIR)/generated/controllers/generated
@@ -6,16 +7,20 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# this would save some flash while being unable to update WBO controller firmware
-DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
+DDEFS += -DEFI_STORAGE_SD=TRUE
+# assign critical LED to a non-existent pin
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# we do not have much Lua RAM, let's drop some fancy functions
+DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
+DDEFS += -DWITH_LUA_PID=FALSE
+DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE
 
-# EGT chip
-#un-comment to enable
-#DDEFS += -DEFI_MAX_31855=TRUE
 
-#see main repo for details on this any many other optional subsystems. We have too many, one has to choose what fits into his choice of stm32
-#DDEFS += -DEFI_ONBOARD_MEMS=TRUE
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DKNOCK_SPECTROGRAM=TRUE
+DDEFS += -DEFI_MAX_31855=TRUE
